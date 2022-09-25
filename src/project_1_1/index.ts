117be73d10bd50b2fe3581e780a85f8cc10b11b2
5knnbdwm <samuel@soeller.dev>
2022-09-25 18:52:30 +0200

    added: small changes

--- src/project_1_1/index.ts
+++ src/project_1_1/index.ts
@@ -1,7 +1,10 @@
 import * as aq from "arquero";
+import fs from "fs";
 
 export async function action() {
-  const dt = await aq.loadCSV("./national/yob2021.csv", { autoType: true });
-  console.log(dt.size);
+  console.log(fs.readdirSync("@/"));
+  // console.log(await fs.readFileSync("./national/yob2021.txt"));
+  // const dt = await aq.loadCSV("./national/yob2021.txt", { autoType: true });
+  // console.log(dt.size);
   console.log("here");
 }
